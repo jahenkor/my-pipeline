0567646196659262b10b8963037e42895a6c77e8
EC2 Default User <ec2-user@ip-172-31-4-166.us-east-2.compute.internal>
2024-08-13 19:25:24 +0000

    init

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -8,21 +8,18 @@ class MyPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+
         synth_step = ShellStep("Synth",
                 input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
                 commands=["npm install -g aws-cdk",
                   "python -m pip install -r requirements.txt",
                   "cdk synth"])
 
-        pipeline =  CodePipeline(self, "Pipeline",
-                        pipeline_name="MyPipeline",
-                        synth=ShellStep("Synth",
-                            input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
-                            commands=["npm install -g aws-cdk",
-                                "python -m pip install -r requirements.txt",
-                                "cdk synth"]))
-
+        pipeline = CodePipeline(self, "Pipeline",
+                pipeline_name="MyPipeline",
+                synth=synth_step)
+        
         stage = pipeline.add_stage(MyPipelineAppStage(self, "test",
-            env=cdk.Environment(account="654654214598", region="us-west-2")))
+            env=cdk.Environment(account="654654214598", region="us-east-2")))
 
-        stage.add_post(ShellStep("validate", input=source, commands=["sh ../tests/validate.sh"],))
+        stage.add_post(ShellStep("validate", input=synth_step, commands=["node test/validate.js"],))
